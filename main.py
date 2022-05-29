702bfe96f42f2d0fbcc75a3aa80d227d8027c261
LuckyBoop <foster.jonah@ufl.edu>
2022-05-29 18:27:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import discord
 import os
+import random
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 client = discord.Client()
 
-@client.event 
+description = '''The Jonah Foster Test Bot'''
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='.', description=description, intents=intents)
+
+@bot.event
 async def on_ready():
-    print('We have logged in as Luckbot'.format(client))
+    print('Logged in as LuckBot')
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
 
 client.run(os.getenv("DISCORD_TOKEN"))
