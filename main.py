fd1a2e91f154f3da9d7e013fa94e533ce3f1862b
LuckyBoop <foster.jonah@ufl.edu>
2022-06-21 15:51:32 -0400

    Switched library to pycord

--- main.py
+++ main.py
@@ -1,33 +1,22 @@
 import discord
 import os
-import random
-from discord.ext import commands
 from dotenv import load_dotenv
+import random
 
 load_dotenv()
-client = discord.Client()
+bot = discord.Bot(debug_guilds=[933551727557373993])
 
 description = '''The Jonah Foster Test Bot'''
 
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix='.', description=description, intents=intents)
-
 @bot.event
 async def on_ready():
-    print('Logged in as LuckBot')
+    print('Luckbot is online')
 
-@bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return
+@bot.slash_command(name = "test", description = "A test command.")
+async def test(ctx):
+    await ctx.respond("Test passed.")
 
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
-
-client.run(os.getenv("DISCORD_TOKEN"))
+bot.run(os.getenv("DISCORD_TOKEN"))
