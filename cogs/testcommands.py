e47d03029c62c421402dc02150816e7337ead713
LuckyBoop <foster.jonah@ufl.edu>
2022-06-22 13:53:39 -0400

    New commands added
    Implemented commands using random and commands that send images.

--- cogs/testcommands.py
+++ cogs/testcommands.py
@@ -1,14 +1,37 @@
 import discord
 from discord.ext import commands
+import random
 
 class testCommands(commands.Cog): # Create the class for the cog
 
     def __init__(self, bot):
         self.bot = bot
     
+    # Sample Text Command
     @discord.slash_command(name = "test", description = "A test command.")
-    async def test(ctx):
+    async def test(self, ctx):
         await ctx.respond("Test passed.")
+
+    #Sample Random Choice Command
+    @discord.slash_command(name = "testrandomchoices", description = "A test command.")
+    async def testrandomchoices(self, ctx):
+        await ctx.respond(random.choice(testing))
+
+    #Sample Image Command
+    @discord.slash_command(name = "testimage", description = "A test command.")
+    async def testimage(self, ctx):
+        await ctx.respond(file=discord.File('C:/Users/jonah/Desktop/LuckBot/photos/cryingmj.jpeg'))
 
 def setup(bot):
     bot.add_cog(testCommands(bot)) # Add the cog to the bot
+
+
+
+
+
+
+
+
+
+
+testing = ["Test1", "Test2", "Test3", "Test4"]
