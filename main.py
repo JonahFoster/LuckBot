b6dec2658398e9b5a53b09db1550c3f9059eab2c
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-02-22 17:33:46 -0500

    Revert "OpenAI implementation"
    This reverts commit 56c5a1d0e2b6f906a6f3a55d61ea1bde605f1672.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import discord
 import os
 from dotenv import load_dotenv
-import sys
 
 load_dotenv()
 bot = discord.Bot(debug_guilds=[933551727557373993])
@@ -11,32 +10,11 @@ description = '''The Jonah Foster Test Bot'''
 intents = discord.Intents.default()
 intents.members = True
 
-def check_version() -> None:
-    import pkg_resources
-
-    # Read the requirements.txt file and add each line to a list
-    with open('requirements.txt') as f:
-        required = f.read().splitlines()
-
-    # For each library listed in requirements.txt, check if the corresponding version is installed
-    for package in required:
-        # Use the pkg_resources library to get information about the installed version of the library
-        package_name, package_verion = package.split('==')
-        installed = pkg_resources.get_distribution(package_name)
-        # Extract the library name and version number
-        name, version = installed.project_name, installed.version
-        # Compare the version number to see if it matches the one in requirements.txt
-        if package != f'{name}=={version}':
-            print(f'{name} version {version} is installed but does not match the requirements')
-            sys.exit();
-
-if __name__ == '__main__': 
-    check_version()
-
 @bot.event
 async def on_ready():
     print('Luckbot is online')
 
 bot.load_extension('cogs.testcommands')
+bot.load_extension('cogs.twitter')
 
 bot.run(os.getenv("DISCORD_TOKEN"))
